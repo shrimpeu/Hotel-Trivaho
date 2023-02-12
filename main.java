22dd05f1dde7cb4cfb57cf4f62f26502f47e8f12
PotatoMochii <119642270+PotatoMochii@users.noreply.github.com>
2023-02-12 22:21:06 +0800

    Update main.java

--- main.java
+++ main.java
@@ -2,3 +2,43 @@ package HotelManagement;
 
 import java.io. *;
 
+public class Menu {
+    
+public static void main(String[] args) throws Exception {
+  BufferedReader input = new BufferedReader   // Input Scanner
+        (new InputStreamReader(System.in));
+  
+  char reply;
+  
+  // Menu
+  System.out.println("[1] Book Reservation");
+  System.out.println("[2] Cancel Reservation");
+  System.out.println("[3] View Reservations");
+  System.out.println("[4] View Available Rooms");
+  System.out.println("[5] Exit");
+  
+  System.out.println("Enter choice: ");
+  reply = input.readLine();
+  
+  switch(reply){
+    case 1:
+      bookreservation();
+      break;
+    case 2:
+      cancelreservation();
+      break:
+    case 3:
+      viewreservation();
+      break:
+    case 4:
+      viewavailroom();
+      break:
+    case 5:
+      exit();
+      break:
+  }
+  
+  
+}
+  
+}
