d4c0a77885c8de716c8750c652ac4ac5f51eae07
MarkJeremiah <108478430+MarkJeremiah@users.noreply.github.com>
2023-02-13 22:44:58 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,70 +1,97 @@
 package HotelManagement;
 
-import java.io. *;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
+
 
 public class main {
-  
-  class globalData {
-   public static String[][] database = new String [30][7];
-  }
-  
-  // Home Method
-static void Home () throws IOException {
-    BufferedReader input = new BufferedReader   // Input Scanner
-        (new InputStreamReader(System.in));
-  
-  int reply;
-  
-  globalData.database [0][0] = "Dion";
-  System.out.println(globalData.database[0][0]);
-  
-  System.out.println("\t ||| Welcome to Hotel Trivaho ||| ");
-  System.out.println("\t |||  Please enjoy your stay! ||| ");  
-    
-  // Menu
-  System.out.println("[1] Dashboard ");
-  System.out.println("[2] Clieht ");
-  System.out.println("[3] Log ");
-  System.out.println("[4] Reservations ");
-  System.out.println("[5] Rooms ");
-  System.out.println("[6] Exit");
-  
-  System.out.println("Enter choice: ");
-  reply = Integer.parseInt(input.readLine());
-  
-  switch(reply){
-    case 1:
-      // Dashboard();
-      break;
-    case 2:
-     //  Client();
-      break;
-    case 3:
-      // Log();
-      break;
-    case 4:
-      // Reservation();
-      break;
-    case 5:
-      // Rooms();
-      break;
-    case 6:
-      // return 0;
-      break;
-   }
- }
 
-static void Dashboard () throws IOException {
-  // for (int i = 0; i <
-  // System.out.println("Available Rooms: ")
-
-}
-  
+class globalData {
+        public static String[][] database = new String [30][7];
+        public static int counter = 1;
+        }  
+	
+	
+static void Home() throws NumberFormatException, IOException {
+	BufferedReader input = new BufferedReader   // Input Scanner
+	        (new InputStreamReader(System.in));
+	Scanner s = new Scanner(System.in);
+	  
+	  int reply;
+	  	  
+	  System.out.println("\t ||| Welcome to Hotel Trivaho ||| ");
+	  System.out.println("\t |||  Please enjoy your stay! ||| ");  
+	    
+	  // Menu
+	  System.out.println("[1] Dashboard ");
+	  System.out.println("[2] Clieht ");
+	  System.out.println("[3] Log ");
+	  System.out.println("[4] Reservations ");
+	  System.out.println("[5] Rooms ");
+	  System.out.println("[6] Exit");
+	  
+	  System.out.print("Enter choice: ");
+	  reply = Integer.parseInt(input.readLine());
  
+	  
+	  switch(reply){
+	    case 1:
+	    	System.out.println(new String(new char[70]).replace("\0", "\r\n"));
+	    	Dashboard();
+	      break;
+	    case 2:
+	    	System.out.println(new String(new char[70]).replace("\0", "\r\n"));
+	     //  Client();
+	      break;
+	    case 3:
+	    	System.out.println(new String(new char[70]).replace("\0", "\r\n"));
+	      // Log();
+	      break;
+	    case 4:
+	    	System.out.println(new String(new char[70]).replace("\0", "\r\n"));
+	      // Reservation();
+	      break;
+	    case 5:
+	    	System.out.println(new String(new char[70]).replace("\0", "\r\n"));
+	      // Rooms();
+	      break;
+	    case 6:
+	    	cls();
+	      break;
+	      
+	      default:
+	    	  System.out.print("Invalid Input");
+	    	  Home();
+	    	  break;
+	   }
+		  
 }
-  // Main Method
-    public static void Main(String[] args) throws IOException {
 
-        Home();
-  
+
+public static void cls() {
+	try {
+		new ProcessBuilder("cmd","/c", "cls").inheritIO().start().waitFor();
+	}catch(Exception E) {
+		System.out.print(E);
+	}
+}
+
+
+public static void main(String[] args) throws Exception {
+		  Home();  
+		  }
+	
+static void Dashboard() {
+	System.out.println("Dashboard");
+	System.out.println("Dashboard");
+	System.out.println("Dashboard");
+	System.out.println("Dashboard");
+	System.out.println("Dashboard");
+	System.out.println("Dashboard");
+	System.out.println("Dashboard");
+}
 }
